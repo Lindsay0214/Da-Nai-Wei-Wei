03fb98d65327c4f86c1404b1f3125cc2fc102ba9
a_u_z <77137373+a-u-z@users.noreply.github.com>
2021-09-28 20:12:01 +0800

    [fix/seeders]將 CoCo 替換成麻古茶坊，將 CoCo 菜單也替換

--- seeders/20210926012944-user-store.js
+++ seeders/20210926012944-user-store.js
@@ -4,17 +4,19 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
+        id: 1000,
         nickname: '',
         password: '$2b$10$TYObVm4oh9XZmwH1Is/ZbO6sygo9zfdsWXPP/M9nNSPvAbJd9IUPu',
         email: 'c@gmail.com',
         creditcard: '0000-0000-0000-0111',
         address: '台北市快樂區無常街111號3樓',
         role: 'shop',
-        brand_name: 'CoCo',
+        brand_name: '麻谷茶坊',
         createdAt: '2021-09-10 06:05:45',
         updatedAt: '2021-09-18 06:05:45',
       },
       {
+        id: 1001,
         nickname: '',
         password: '$2b$10$TYObVm4oh9XZmwH1Is/ZbO6sygo9zfdsWXPP/M9nNSPvAbJd9IUPu',
         email: '5@gmail.com',
@@ -26,6 +28,7 @@ module.exports = {
         updatedAt: '2021-09-18 06:05:45',
       },
       {
+        id: 1002,
         nickname: '',
         password: '$2b$10$TYObVm4oh9XZmwH1Is/ZbO6sygo9zfdsWXPP/M9nNSPvAbJd9IUPu',
         email: 'n@gmail.com',
